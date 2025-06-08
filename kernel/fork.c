7bc4e49a5e8ea3e4b07869159070e9beb8e06f82
John Galt <johngaltfirstrun@gmail.com>
2025-06-08 08:21:29 -0400

    Revert "Squash revert of SimpleLMK:"
    This reverts commit dd1c8b536b6edd02827fbe89ddfd693db8e92f51.
    
    Change-Id: I5451483fc8b5f2efd2f7ac03df55083aac257ccc
    Signed-off-by: klozz <carlosj@klozz.dev>

--- kernel/fork.c
+++ kernel/fork.c
@@ -101,6 +101,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/dma-buf.h>
 #include <linux/cpu_boost.h>
+#include <linux/simple_lmk.h>
 
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -1313,6 +1314,7 @@ static inline void __mmput(struct mm_struct *mm)
 	ksm_exit(mm);
 	khugepaged_exit(mm); /* must run before exit_mmap */
 	exit_mmap(mm);
+	simple_lmk_mm_freed(mm);
 	mm_put_huge_zero_page(mm);
 	set_mm_exe_file(mm, NULL);
 	put_dmabuf_info(mm->abi_extend->dmabuf_info);
--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -1140,6 +1140,10 @@ bool out_of_memory(struct oom_control *oc)
 {
 	unsigned long freed = 0;
 
+	/* Return true since Simple LMK automatically kills in the background */
+	if (IS_ENABLED(CONFIG_ANDROID_SIMPLE_LMK))
+		return true;
+
 	if (oom_killer_disabled)
 		return false;
 
@@ -1230,6 +1234,7 @@ void pagefault_out_of_memory(void)
 		pr_warn("Huh VM_FAULT_OOM leaked out to the #PF handler. Retrying PF\n");
 }
 
+#ifndef CONFIG_ANDROID_SIMPLE_LMK
 SYSCALL_DEFINE2(process_mrelease, int, pidfd, unsigned int, flags)
 {
 #ifdef CONFIG_MMU
@@ -1293,6 +1298,7 @@ put_task:
 	return -ENOSYS;
 #endif /* CONFIG_MMU */
 }
+#endif
 
 void add_to_oom_reaper(struct task_struct *p)
 {
